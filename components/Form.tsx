d77f963b4b124571ea6316298ea83ebe788af353
nikolamirilo <nikolamirilo@gmail.com>
2023-11-10 23:31:29 +0100

    adding function for addition of product

--- components/Form.tsx
+++ components/Form.tsx
@@ -3,14 +3,16 @@ import Image from "next/image";
 import React, { useRef, useState } from "react";
 import logo from "../public/MontreLogoTransparent.png";
 import { CloudinaryResponse } from "@/typescript/interfaces";
+import { revalidateData } from "@/helpers";
 
 const Form: React.FC = () => {
   const [displayImage, setDisplayImage] = useState("");
-  const categoryInput = useRef<HTMLInputElement>(null);
+  const categoryInput = useRef<HTMLSelectElement>(null);
   const priceInput = useRef<HTMLInputElement>(null);
   const titleInput = useRef<HTMLInputElement>(null);
   const descriptionInput = useRef<HTMLTextAreaElement>(null);
-  const fileInput = useRef<HTMLInputElement>(null);
+  const imagesInput = useRef<HTMLInputElement>(null);
+  const images: any = [];
   //Display different image every time new image is changed
   const handleInputImageChange = (e: any) => {
     const file = e?.target?.files?.[0];
@@ -23,75 +25,82 @@ const Form: React.FC = () => {
     }
   };
 
-  const formData = new FormData();
-
   const handleFormSubmit = async (e: any) => {
     e.preventDefault();
-    const file = await fileInput?.current?.files?.[0];
-    if (!file) {
+    const files = await imagesInput?.current?.files;
+    if (!files) {
       console.error("No file selected.");
       return;
     }
-    formData.append("file", file);
-    console.log(file);
-    formData.append("upload_preset", "products");
-    var image = "";
-    try {
-      const response = await fetch(
-        "https://api.cloudinary.com/v1_1/montre-cloudinary/image/upload",
+    const formData = new FormData();
+
+    for (let i = 0; i <= files.length; i++) {
+      formData.append("file", files[i]);
+      formData.append("upload_preset", "products");
+      try {
+        const response = await fetch(
+          "https://api.cloudinary.com/v1_1/montre-cloudinary/image/upload",
+          {
+            method: "POST",
+            body: formData,
+          }
+        );
+
+        if (!response.ok) {
+          throw new Error(`Image upload failed: ${response.statusText}`);
+        }
+
+        // Now, extract and parse the JSON response correctly
+        const uploadImage: CloudinaryResponse = await response.json();
+        await images.push(uploadImage.url);
+        formData.delete("file");
+        // Rest of your code here, if needed
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    }
+    const uploadData = {
+      title: titleInput.current!.value,
+      price: priceInput.current!.value,
+      category: categoryInput.current!.value,
+      description: descriptionInput.current!.value,
+      images: images,
+    };
+    console.log(uploadData);
+    if (uploadData) {
+      await fetch(
+        "https://eu-central-1.aws.data.mongodb-api.com/app/data-eagdn/endpoint/data/v1/action/insertOne",
         {
           method: "POST",
-          body: formData,
+          headers: {
+            "api-key":
+              "QZCIcGQluFrPOHPAMvvrk9taEjiMifFAtrjGnmlM2efolfQjELuTbLdJTWhbhuYQ",
+            "Content-Type": "application/json",
+            "Access-Control-Request-Headers": "*",
+          },
+          body: JSON.stringify({
+            dataSource: "MainCluster",
+            database: "Store",
+            collection: "products",
+            document: uploadData,
+          }),
         }
-      );
-
-      if (!response.ok) {
-        throw new Error(`Image upload failed: ${response.statusText}`);
-      }
-
-      // Now, extract and parse the JSON response correctly
-      const uploadImage: CloudinaryResponse = await response.json();
-      image = uploadImage.url;
-      // Rest of your code here, if needed
-    } catch (error) {
-      console.error("Error:", error);
+      ).then((response) => {
+        console.log(response);
+        //Set Initial data
+        categoryInput.current!.value = "";
+        priceInput.current!.value = "";
+        titleInput.current!.value = "";
+        descriptionInput.current!.value = "";
+        setDisplayImage("");
+        if (response.ok) {
+          revalidateData();
+          alert("Vas odgovor je zabelezen");
+        } else {
+          console.log(response.statusText);
+        }
+      });
     }
-
-    const uploadData = {
-      category: categoryInput.current!.value,
-      price: priceInput.current!.value,
-      title: titleInput.current!.value,
-      description: descriptionInput.current!.value,
-      likes: 0,
-      image: image,
-    };
-
-    console.log(uploadData);
-    //Replace with Mongo DB API
-    // await fetch("/api/posts/create-post", {
-    //   method: "POST",
-    //   headers: {
-    //     Accept: "application/json",
-    //     "Content-Type": "application/json",
-    //   },
-    //   body: JSON.stringify(uploadData),
-    // }).then((response) => {
-    //   console.log(response);
-
-    //   //Set Initial data
-    //   categoryInput.current!.value = "";
-    //   priceInput.current!.value = "";
-    //   titleInput.current!.value = "";
-    //   descriptionInput.current!.value = "";
-    //   setDisplayImage("");
-
-    //   if (response.ok) {
-    //     revalidateData();
-    //     alert("Vas odgovor je zabelezen");
-    //   } else {
-    //     console.log(response.statusText);
-    //   }
-    // });
   };
 
   return (
@@ -139,7 +148,7 @@ const Form: React.FC = () => {
               htmlFor="price"
               className="block text-sm font-medium leading-5 text-gray-700"
             >
-             Cena proizvoda:
+              Cena proizvoda:
             </label>
             <div className="mt-1">
               <input
@@ -160,16 +169,17 @@ const Form: React.FC = () => {
               Kategorija proizvoda:
             </label>
             <div className="mt-1">
-            <select
-          id="category"
-          name="category"
-          className="w-full h-10 border-2 text-sm focus:border-amber-500 focus:outline-none rounded-lg cursor-pointer px-2 md:px-3 py-0 md:py-1 text-gray-900"
-        >
-          <option value="Premium">Premium</option>
-          <option value="Mid">Mid</option>
-          <option value="Casual">Casual</option>
-          <option value="Sport">Sport</option>
-        </select>
+              <select
+                ref={categoryInput}
+                id="category"
+                name="category"
+                className="w-full h-10 border-2 text-sm focus:border-amber-500 focus:outline-none rounded-lg cursor-pointer px-2 md:px-3 py-0 md:py-1 text-gray-900"
+              >
+                <option value="Premium">Premium</option>
+                <option value="Mid">Mid</option>
+                <option value="Casual">Casual</option>
+                <option value="Sport">Sport</option>
+              </select>
             </div>
           </div>
           <div>
@@ -201,7 +211,7 @@ const Form: React.FC = () => {
               <div className="flex items-center justify-center w-full">
                 <label
                   htmlFor="image-input"
-                  className="flex relative flex-col items-center justify-center bg-center bg-cover w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-white hover:border-amber-500"
+                  className="flex relative flex-col items-center justify-center bg-center bg-cover w-full h-96 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-white hover:border-amber-500"
                 >
                   {displayImage !== "" ? (
                     <Image
@@ -241,8 +251,9 @@ const Form: React.FC = () => {
                   </div>
                   <input
                     onChange={handleInputImageChange}
+                    multiple={true}
                     id="image-input"
-                    ref={fileInput}
+                    ref={imagesInput}
                     name="image-input"
                     type="file"
                     accept="image/*"
@@ -256,12 +267,12 @@ const Form: React.FC = () => {
             <button
               type="submit"
               id="submit-button"
-              className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-600 hover:bg-green-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:border-amber-500 focus:border-2"
+              className="uppercase w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-600 hover:bg-green-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:border-amber-500 focus:border-2"
               onClick={(e: any) => {
                 handleFormSubmit(e);
               }}
             >
-              Submit Post
+              Dodaj proizvod
             </button>
           </div>
         </form>
