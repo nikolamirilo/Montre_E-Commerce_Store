c66e87db4fb6a953222ed249242ef16814097ba7
Nikola.Mirilo <nikola.mirilo@adacta-fintech.com>
2023-11-15 12:27:56 +0100

    added mongo client

--- components/Products.tsx
+++ components/Products.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import Card from "./Card";
 import { Product } from "@/typescript/interfaces";
 import { getAllProducts } from "@/actions/server/products";
-import Search from "./Search";
 
 export const revalidate = 0;
 export const dynamic = "force-dynamic";
