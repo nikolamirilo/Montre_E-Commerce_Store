34ff1f715a89960753b5013aab43b8d50c3f9c51
Nikola.Mirilo <nikola.mirilo@adacta-fintech.com>
2023-11-29 14:14:23 +0100

    added urls

--- app/auth/[action]/page.tsx
+++ app/auth/[action]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn, SignUp } from "@clerk/nextjs"
 const Auth = ({ params }: { params: { action: string } }) => {
   return (
     <main className="flex mt-10 flex-row justify-center items-center">
-      {params.action == "log-in" ? <SignIn /> : <SignUp />}
+      {params.action == "log-in" ? <SignIn afterSignInUrl="/" /> : <SignUp afterSignUpUrl="/" />}
     </main>
   )
 }
