c43b5a25e5d0115176d52ec0c5bc1bdd6abe5fcf
Nikola.Mirilo <nikola.mirilo@adacta-fintech.com>
2023-11-29 15:26:09 +0100

    restrcutured cart component

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -12,10 +12,10 @@ const ShoppingCart = async () => {
     <main>
       <div className="min-h-screen bg-white py-20 h-fit">
         <h1 className="mb-10 text-center text-3xl uppercase font-bold">Proizvodi u korpi</h1>
-        <div className="mx-auto max-w-5xl justify-center px-6 md:flex md:space-x-6 xl:px-0">
-          <div className="rounded-lg md:w-2/3">
-            {mongoUser?.cart.length > 0 ? (
-              mongoUser?.cart?.map(async (item: string) => {
+        {mongoUser?.cart.length > 0 ? (
+          <div className="mx-auto max-w-5xl justify-center px-6 md:flex md:space-x-6 xl:px-0">
+            <div className="rounded-lg md:w-2/3">
+              {mongoUser?.cart?.map(async (item: string) => {
                 const product = await getSingleProduct(item)
                 return (
                   <CartItem
@@ -26,35 +26,35 @@ const ShoppingCart = async () => {
                     image={product.images[0]}
                   />
                 )
-              })
-            ) : (
-              <h2 className="text-2xl font-semibold">Nema ni jedan proizvod u korpi</h2>
-            )}
-          </div>
-          <div className="mt-6 h-full rounded-lg border bg-white p-6 shadow-md md:mt-0 md:w-1/3">
-            <div className="mb-2 flex justify-between">
-              <p className="text-gray-700">Cena satova:</p>
-              <p className="text-gray-700">$129.99</p>
+              })}
             </div>
-            <div className="flex justify-between">
-              <p className="text-gray-700">Dostava</p>
-              <p className="text-gray-700">$4.99</p>
-            </div>
-            <hr className="my-4" />
-            <div className="flex justify-between">
-              <p className="text-lg font-bold">Total</p>
-              <div className="">
-                <p className="mb-1 text-lg font-bold">$134.98 USD</p>
-                <p className="text-sm text-gray-700">including VAT</p>
+            <div className="mt-6 h-full rounded-lg border bg-white p-6 shadow-md md:mt-0 md:w-1/3">
+              <div className="mb-2 flex justify-between">
+                <p className="text-gray-700">Cena satova:</p>
+                <p className="text-gray-700">$129.99</p>
               </div>
+              <div className="flex justify-between">
+                <p className="text-gray-700">Dostava</p>
+                <p className="text-gray-700">$4.99</p>
+              </div>
+              <hr className="my-4" />
+              <div className="flex justify-between">
+                <p className="text-lg font-bold">Total</p>
+                <div className="">
+                  <p className="mb-1 text-lg font-bold">$134.98 USD</p>
+                  <p className="text-sm text-gray-700">including VAT</p>
+                </div>
+              </div>
+              <Link
+                className="mt-6 w-full rounded-md bg-amber-500 py-2 px-4 font-medium text-amber-50 hover:bg-amber-600"
+                href="/order">
+                Poruči
+              </Link>
             </div>
-            <Link
-              className="mt-6 w-full rounded-md bg-amber-500 py-2 px-4 font-medium text-amber-50 hover:bg-amber-600"
-              href="/order">
-              Poruči
-            </Link>
           </div>
-        </div>
+        ) : (
+          <h2 className="text-2xl font-semibold">Nema ni jedan proizvod u korpi</h2>
+        )}
       </div>
     </main>
   )
