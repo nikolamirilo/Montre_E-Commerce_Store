572520cb7f1c35ddf2486434aaf27c08593c4d53
Nikola.Mirilo <nikola.mirilo@adacta-fintech.com>
2023-12-01 11:58:36 +0100

    removed bg from links

--- app/manifest.ts
+++ app/manifest.ts
@@ -1,19 +1,19 @@
-import { MetadataRoute } from 'next'
- 
+import { MetadataRoute } from "next"
+
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: 'Montre Satovi',
-    short_name: 'Montre Satovi',
-    description: 'Online prodavnica satova',
-    start_url: '/',
-    display: 'standalone',
-    background_color: '#fff',
-    theme_color: '#fff',
+    name: "Montre Satovi",
+    short_name: "Montre Satovi",
+    description: "Online prodavnica satova",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#fff",
+    theme_color: "#fff",
     icons: [
       {
-        src: '/favicon.ico',
-        sizes: 'any',
-        type: 'image/x-icon',
+        src: "/favicon.ico",
+        sizes: "any",
+        type: "image/x-icon",
       },
     ],
   }
