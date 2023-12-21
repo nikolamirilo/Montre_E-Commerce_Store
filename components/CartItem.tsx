3af2b09e3d449a0d7759b4f3bb7aa706082dd1cd
nikolamirilo <nikolamirilo@gmail.com>
2023-12-22 00:42:12 +0100

    fixed calculations

--- components/CartItem.tsx
+++ components/CartItem.tsx
@@ -20,18 +20,13 @@ const CartItem = ({
     await deleteCartItem(uid, _id)
     revalidateData()
   }
-  const setCountInfo = async (operation: string) => {
-    if (operation == "addition") {
-      await updateItemCount(uid, _id, quantity + 1)
-    } else {
-      await updateItemCount(uid, _id, quantity - 1)
-    }
+  const setCountInfo = async (q: number) => {
+    await updateItemCount(uid, _id, q)
     revalidateData()
   }
   const calculatedPrice = (isOnDiscount ? discountedPrice : price) * quantity
   return (
     <div className="justify-between mb-6 rounded-lg bg-white p-6 shadow-md sm:flex sm:justify-start relative">
-      {/* <input type="checkbox" className="absolute top-0 left-0 w-5 h-5 cursor-pointer" /> */}
       <button onClick={handleDeleteCartItem} className="absolute top-0 right-0">
         <IoMdClose size={30} className="hover:fill-red-500" />
       </button>
@@ -39,27 +34,35 @@ const CartItem = ({
       <div className="sm:ml-4 sm:flex sm:w-full sm:justify-between">
         <div className="mt-5 sm:mt-0">
           <h2 className="text-lg font-bold text-gray-900">{title}</h2>
-          <p className="mt-1 text-lg text-gray-700">{category == "man" ? "Muški" : "Ženski"}</p>
+          <p className="mt-1 text-lg text-gray-700">{category === "man" ? "Muški" : "Ženski"}</p>
           <p className="mt-1 text-lg text-gray-700">{productClass}</p>
         </div>
         <div className="mt-4 flex flex-row-reverse md:flex-col sm:mt-0 h-full justify-between md:justify-around items-center">
           <div className="flex items-center border-gray-200">
             <button
               onClick={() => {
-                setCountInfo("substraction")
+                setCountInfo(1)
               }}
-              className="cursor-pointer text-xl rounded-l bg-gray-100 py-[2px] px-3.5 duration-100 hover:bg-amber-500 hover:text-amber-50">
-              -
+              className={`cursor-pointer text-xl rounded-l py-[2px] px-3.5 duration-100 hover:bg-amber-500 border border-gray-300 bg-gray-100`}>
+              1
             </button>
-            <span className="h-8 w-8 border text-xl bg-white text-center outline-none">
-              {quantity}
-            </span>
             <button
               onClick={() => {
-                setCountInfo("addition")
+                setCountInfo(2)
               }}
-              className="cursor-pointer text-xl rounded-r bg-gray-100 py-[2px] px-3 duration-100 hover:bg-amber-500 hover:text-amber-50">
-              +
+              className={`cursor-pointer text-xl rounded-l py-[2px] px-3.5 duration-100 hover:bg-amber-500 border border-gray-300 bg-gray-100 
+               
+              `}>
+              2
+            </button>
+            <button
+              onClick={() => {
+                setCountInfo(3)
+              }}
+              className={`cursor-pointer text-xl rounded-l py-[2px] px-3.5 duration-100 hover:bg-amber-500 border border-gray-300 bg-gray-100
+          
+              `}>
+              3
             </button>
           </div>
           <div className="flex items-center space-x-4" id="price">
