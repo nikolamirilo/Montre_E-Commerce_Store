bfb1099679a09bbda05edfefb3eb5f759f434be0
nikolamirilo <nikolamirilo@gmail.com>
2024-03-02 14:43:15 +0100

    fixed style of products

--- components/Products.tsx
+++ components/Products.tsx
@@ -24,10 +24,7 @@ const Products: React.FC<ProductsProps> = async ({ query, title, subtitle, type 
         )}
       </div>
       <Search type={type} params={query} />
-      <div
-        className={`px-0 flex flex-wrap w-fit justify-center gap-4 items-center ${
-          products.length >= 4 && "cards-container"
-        } max-w-[1500px] mx-auto`}>
+      <div className="px-0 flex flex-wrap w-fit justify-center gap-4 items-center cards-container max-w-[1500px] mx-auto">
         {products
           ? products.map((product: Product, idx: number) => {
               if (
