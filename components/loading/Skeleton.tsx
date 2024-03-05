0e845021f6f6b434fe3cc209773a86f07d6bf2cc
Nikola.Mirilo <nikola.mirilo@adacta-fintech.com>
2024-03-05 13:17:41 +0100

    fixed loader 2

--- components/loading/Skeleton.tsx
+++ components/loading/Skeleton.tsx
@@ -1,3 +1,5 @@
+import Search from "../helpers/Search"
+
 export function Skeleton({ className }: { className: string }) {
   return <div className={`bg-slate-200 motion-safe:animate-pulse rounded ${className}`} />
 }
