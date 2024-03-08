5ce66fda2a90170a30d3d83e75764cabbb2b9273
Nikola.Mirilo <nikola.mirilo@adacta-fintech.com>
2024-03-08 13:40:31 +0100

    added dynamic to products route

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,6 +1,8 @@
 import { getAllProducts } from "@/actions/server/products"
 import { NextRequest, NextResponse } from "next/server"
 
+export const dynamic = "force-dynamic"
+
 export async function GET(request: NextRequest) {
   try {
     const allProducts = await getAllProducts({})
