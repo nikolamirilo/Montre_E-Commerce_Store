49c6589ff8a848f857f77eb529ebd9acc6fc0467
Nikola.Mirilo <nikola.mirilo@adacta-fintech.com>
2024-03-13 21:55:41 +0100

    fixed style on single blog page

--- app/blogs/[id]/page.tsx
+++ app/blogs/[id]/page.tsx
@@ -2,15 +2,14 @@ import data from "@/data/blogs.json"
 import Image from "next/image"
 const Blog = ({ params }: { params: { id: string } }) => {
   const id = params.id
-  console.log(params)
   const singleBlog = data.blogs.find((blog) => blog.blogCode === id)
   if (singleBlog)
     return (
-      <div className="container max-w-[1200px] flex flex-col justify-center items-center gap-5">
-        <div className="flex flex-row gap-3">
+      <div className="container max-w-[1200px] flex flex-col justify-center items-center gap-5 w-11/12 py-10">
+        <div className="flex flex-col lg:flex-row gap-3">
           <div className="flex flex-col justify-center gap-5">
-            <h1 className="text-3xl font-bold">{singleBlog.title}</h1>
-            <h2 className="text-2xl">{singleBlog.subtitle}</h2>
+            <h1 className="text-2xl lg:text-3xl font-bold">{singleBlog.title}</h1>
+            <h2 className="text-xl lg:text-2xl">{singleBlog.subtitle}</h2>
             <p>{singleBlog.description}</p>
             <span className="font-semibold flex flex-row gap-3 text-lg justify-center items-center w-fit">
               <Image
@@ -37,7 +36,7 @@ const Blog = ({ params }: { params: { id: string } }) => {
             return (
               <div
                 className={`${
-                  idx % 2 == 0 ? "flex-row-reverse" : "flex-row"
+                  idx % 2 == 0 ? "lg:flex-row-reverse flex-col" : "lg:flex-row flex-col"
                 } flex justify-center items-center gap-2 ${
                   !item.text && item.subheading ? "flex-col" : "flex-row"
                 }`}
@@ -46,15 +45,15 @@ const Blog = ({ params }: { params: { id: string } }) => {
                   {item.subheading && (
                     <h3
                       className={`font-semibold w-full text-xl ${
-                        !item.text ? "text-center" : idx % 2 == 0 ? "text-right" : "text-left"
-                      } ${!item.image ? "!text-left w-6/12" : ""}`}>
+                        !item.text ? "text-center" : idx % 2 == 0 ? "lg:text-right" : "text-left"
+                      } ${!item.image ? "!text-left w-full lg:w-6/12" : ""}`}>
                       {item.subheading}
                     </h3>
                   )}
                   {item.text && (
                     <p
-                      className={`${idx % 2 == 0 ? "text-right" : "text-left"} ${
-                        !item.image ? "!text-left w-6/12" : ""
+                      className={`${idx % 2 == 0 ? "lg:text-right" : "text-left"} ${
+                        !item.image ? "!text-left w-full lg:w-6/12" : ""
                       }`}>
                       {item.text}
                     </p>
