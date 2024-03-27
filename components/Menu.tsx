a2d0f3d3ee84770f53d7f108412cc0c53cf931ff
Nikola.Mirilo <nikola.mirilo@adacta-fintech.com>
2024-03-27 17:45:09 +0100

    fixed problem in the menu

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,19 +1,18 @@
 import { APP_URL } from "@/constants"
 import { fetchData } from "@/helpers/client"
-import { currentUser } from "@clerk/nextjs"
+import { auth } from "@clerk/nextjs"
 import React from "react"
 import Sidebar from "./Sidebar"
 
 const Menu: React.FC = async () => {
-  const user = await currentUser()
+  const { userId }: { userId: string | null } = auth()
   var cartItemsCount: number = 0
   var mongoUser: any = {}
-  if (user) {
-    const uid = user?.id
+  if (userId) {
     mongoUser = await fetchData(`${APP_URL}/api/users/single-user`, {
       method: "POST",
       cache: "force-cache",
-      body: JSON.stringify({ uid: uid }),
+      body: JSON.stringify({ uid: userId }),
       tags: ["users"],
     })
     cartItemsCount = mongoUser?.cart?.length > 0 ? mongoUser?.cart?.length : 0
